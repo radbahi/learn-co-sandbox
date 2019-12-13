87b34a08b8bdb7c6f0ed7f6ca81442e259a383df
Radouane Bahi <radbahi@gmail.com>
2019-12-13 16:04:34 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,28 @@
+
+  class FunnyBots  
+ 
+    attr_accessor :name, :quotes  
+ 
+    @@bots = []
+ 
+    def initialize(name, quotes)
+      @name = name
+      @quotes = quotes
+      @@bots << self
+    end
+ 
+   def random_quote
+      self.quotes.sample
+    end
+ 
+    def self.bots
+     @@bots
+    end
+ 
+end
+ 
+  archer = FunnyBots.new("Archer", ["Danger Zone!", "Read a book", "The cumulative hangover would literally kill me"])
+  
+  testbot = FunnyBots.new("Lois", ["This is my only quote"])
+  
+ p "We are #{FunnyBots.bots}"
